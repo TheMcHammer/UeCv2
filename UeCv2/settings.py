8b56111b26d696e43ef4d7a009c7943268ef95ed
CONcode <klaudeon@gmail.com>
2020-07-14 10:00:14 +0300

    added Dotenv in settings

--- UeCv2/settings.py
+++ UeCv2/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import dotenv
 import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -30,7 +31,7 @@ STATICFILES_DIRS = (
 )
 
 dotenv_path = os.path.join(BASE_DIR, '.env')
-load_dotenv(dotenv_path)
+dotenv.load_dotenv(dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
